6d3b28a75b43d1ba8fa341bf5564d793908c06fd
englishgeneralpaperptes8021 <engenppr.8021@gmail.com>
2026-08-10 10:52:17 +0800

    Update newgeo.py

--- newgeo.py
+++ newgeo.py
@@ -31,7 +31,7 @@ st.markdown("""
     <style>
     /* 1. Main Page Background */
     .stApp, [data-testid="stAppViewContainer"] {
-        background-color: #DCF863 !important;
+        background-color: #09C875 !important;
     }
     
     /* 2. Sidebar Background */
