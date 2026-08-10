9e3a5f376480e2ab4203f06ecd8870f881c9c5b4
englishgeneralpaperptes8021 <engenppr.8021@gmail.com>
2026-08-10 11:12:22 +0800

    Update newgeo.py

--- newgeo.py
+++ newgeo.py
@@ -52,7 +52,7 @@ st.markdown("""
     .stSelectbox select,
     textarea {
         background-color: #EFA8F0 !important;
-        color: #27BEF5 !important;
+        color: #A5C809 !important;
         border-radius: 10px !important;
         border: 5px solid #A5C809 !important;
     }
